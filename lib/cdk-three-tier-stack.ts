00fd828ae44fb2033fea5a65f28654d9f460a9ff
Florian Clanet <florianclanet@gmail.com>
2021-09-11 00:34:12 +0200

    feat|create vpc

--- lib/cdk-three-tier-stack.ts
+++ lib/cdk-three-tier-stack.ts
@@ -1,10 +1,15 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
 
 export class CdkThreeTierStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-    
+    // Defining a VPC for the application
+    const vpc = new ec2.Vpc(this, 'AppVPC', {
+      cidr: '10.0.0.0/16'
+
+    });
+
   }
 }
--- test/cdk-three-tier.test.ts
+++ test/cdk-three-tier.test.ts
@@ -2,18 +2,19 @@ import { expect as expectCDK, matchTemplate, MatchStyle, haveResource } from '@a
 import * as cdk from '@aws-cdk/core';
 import * as CdkThreeTier from '../lib/cdk-three-tier-stack';
 
-test('Empty Stack', () => {
+const stack = new cdk.Stack();
+
+test('Not Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
     const stack = new CdkThreeTier.CdkThreeTierStack(app, 'MyTestStack');
     // THEN
-    expectCDK(stack).to(matchTemplate({
+    expectCDK(stack).notTo(matchTemplate({
       "Resources": {}
     }, MatchStyle.EXACT))
 });
 
 test('VPC is created', () => {
-  const stack = new cdk.Stack();
-
-  expectCDK(stack).to(haveResource('AWS::EC2::VPC'));
+  const myStack = new CdkThreeTier.CdkThreeTierStack(stack, 'MyTestStack');
+  expectCDK(myStack).to(haveResource('AWS::EC2::VPC'));
 });
