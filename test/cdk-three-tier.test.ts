e6594a10935df80f0c787ad731a5b3494c1177bb
Florian Clanet <florianclanet@gmail.com>
2021-09-14 23:44:36 +0200

    tests|add test for database security group

--- test/cdk-three-tier.test.ts
+++ test/cdk-three-tier.test.ts
@@ -1,4 +1,4 @@
-import { expect as expectCDK, matchTemplate, MatchStyle, haveResource, countResources } from '@aws-cdk/assert';
+import { expect as expectCDK, matchTemplate, MatchStyle, haveResource, haveResourceLike, countResources } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as CdkThreeTier from '../lib/cdk-three-tier-stack';
 
@@ -25,4 +25,18 @@ test('VPC has the right number of resources' ,() => {
   expectCDK(myStack).to(countResources('AWS::EC2::NatGateway', 2));
   expectCDK(myStack).to(countResources('AWS::EC2::RouteTable', 6));
   expectCDK(myStack).to(countResources('AWS::EC2::InternetGateway', 1));
+});
+
+test('VPC has a security group for Database', () => {
+  expectCDK(myStack).to(haveResourceLike('AWS::EC2::SecurityGroup',{
+    SecurityGroupEgress: [
+      {"CidrIp":"10.0.64.0/19","Description":"from 10.0.64.0/19:ALL PORTS","FromPort":0,"IpProtocol":"tcp","ToPort":65535},
+      {"CidrIp":"10.0.96.0/19","Description":"from 10.0.96.0/19:ALL PORTS","FromPort":0,"IpProtocol":"tcp","ToPort":65535}
+  ],
+    SecurityGroupIngress: [
+      { "CidrIp": "10.0.64.0/19", "Description": "10.0.64.0/19:3306", "FromPort": 3306, "IpProtocol": "tcp", "ToPort": 3306 },
+      { "CidrIp": "10.0.96.0/19", "Description": "10.0.96.0/19:3306", "FromPort": 3306, "IpProtocol": "tcp", "ToPort": 3306 },
+    ],
+    VpcId: {"Ref": "AppVPCB7733741"}
+  }));
 });
